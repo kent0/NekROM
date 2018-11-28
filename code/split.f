2c15d4dd58a8f16df133542a664c87435f87ceeb
Kento Kaneko <kaneko2@illinois.edu>
2018-11-28 10:42:38 -0600

    added split.f

---
+++ code/split.f
@@ -0,0 +1,47 @@
+c-----------------------------------------------------------------------
+      subroutine split_domain(domain,nsplit)
+
+      include 'SIZE'
+
+      integer domain(lx1,ly1,lz1,lelt)
+
+      lxyz=lx1*ly1*lz1
+      n=lxyz*nelv
+
+      call izero(domain,n)
+
+      if (ldim.eq.2) then
+         xmax=glmax(x,n)
+         xmin=glmax(x,n)
+
+         do i=1,nelv
+            isplit=1+integer(nsplit*(xm1(2,2,1,i)-xmin)/(xmax-xmin))
+            call ifill(domain(1,1,1,i),isplit,lxyz)
+         enddo
+      else
+         zmax=glmax(z,n)
+         zmin=glmax(z,n)
+
+         do i=1,nelv
+            isplit=1+integer(nsplit*(zm1(2,2,1,i)-zmin)/(zmax-zmin))
+            call ifill(domain(1,1,1,i),isplit,lxyz)
+         enddo
+      endif
+
+      return
+      end
+c-----------------------------------------------------------------------
+      subroutine domain_mask(dmask,domain,isplit)
+
+      include 'SIZE'
+
+      real dmask(lx1*ly1*lz1*lelt)
+      integer domain(lx1*ly1*lz1*lelt)
+
+      do i=1,lx1*ly1*lz1*nelv
+         dmask(i)=1-min(abs(domain(i)-isplit),1)
+      enddo
+
+      return
+      end
+c-----------------------------------------------------------------------
