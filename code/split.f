c3f84f1c61e7309f9d9aa3dbc0e075c879bbb01d
Kento Kaneko <kaneko2@illinois.edu>
2018-12-06 18:56:30 -0600

    implement masking subroutines

--- code/split.f
+++ code/split.f
@@ -32,16 +32,97 @@ c-----------------------------------------------------------------------
       return
       end
 c-----------------------------------------------------------------------
-      subroutine domain_mask(dmask,domain,isplit)
+      subroutine domain_mask(dmask,isplit,nsplit)
 
       include 'SIZE'
       include 'GEOM'
 
       real dmask(lx1*ly1*lz1*lelt)
-      integer domain(lx1*ly1*lz1*lelt)
+
+      n=lx1*ly1*lz1*nelv
+      one=1.
+      pi=4.*atan(one)
+
+      if (nsplit.eq.1) then
+         if (isplit.eq.1) then
+            call rone(dmask,n)
+         else
+            call rzero(dmask,n)
+         endif
+         if (nio.eq.0) write (6,*) 'warning: nsplit is 1'
+         return
+      endif
+
+      xmin=glmin(xm1,n)
+      xmax=glmax(xm1,n)
+
+      dx=(xmax-xmin)/real(nsplit-1)
+      dxi=1./dx
+      x0=dx*real(isplit-1)
+      write (6,*) 'xs',xmin,xmax,real(nsplit-1)
+      write (6,*) 'x0=',x0
+      write (6,*) 'dx=',dx
 
       do i=1,lx1*ly1*lz1*nelv
-         dmask(i)=1-min(abs(domain(i)-isplit),1)
+         x=xm1(i,1,1,1)
+         if (abs(x-x0).lt.dx) then
+            dmask(i)=((1.+cos(pi*(x-x0)*dxi))*.5)
+         else
+            dmask(i)=0.
+         endif
+      enddo
+
+      return
+      end
+c-----------------------------------------------------------------------
+      subroutine domain_mask_p(dmask,isplit,nsplit)
+
+      include 'SIZE'
+      include 'GEOM'
+
+      real dmask(lx1*ly1*lz1*lelt)
+
+      n=lx1*ly1*lz1*nelv
+      one=1.
+      pi=4.*atan(one)
+
+      if (nsplit.eq.1) then
+         if (isplit.eq.1) then
+            call rone(dmask,n)
+         else
+            call rzero(dmask,n)
+         endif
+         if (nio.eq.0) write (6,*) 'warning: nsplit is 1'
+         return
+      endif
+
+      xmin=glmin(xm1,n)
+      xmax=glmax(xm1,n)
+
+      dx=1./real(nsplit)
+      dxi=1./dx
+      x0=dx*real(isplit-1)
+
+      do i=1,lx1*ly1*lz1*nelv
+         x=xm1(i,1,1,1)
+         xp=(x-xmin)/(xmax-xmin)
+         d=min(abs(xp-x0),abs(xp-x0-1.))
+         if (isplit.eq.1) then
+            if (xp.lt.dx) then
+               dmask(i)=(1.+cos(pi*xp*dxi))*.5
+            elseif (xp.gt.(1.-dx)) then
+               dmask(i)=(1.+cos(pi*(1.-xp)*dxi))*.5
+            else
+               dmask(i)=0.
+            endif
+         else
+            if (abs(x0-xp).lt.dx) then
+               d=min(x0-xp,x0+1.-xp)
+               dmask(i)=(1.+cos(pi*d*dxi))*.5
+            else
+               dmask(i)=0.
+            endif
+         endif
       enddo
 
       return
