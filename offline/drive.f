6742e40523a45297801e5fbbcfc0dcd2439ff738
Kento Kaneko <kaneko2@illinois.edu>
2020-02-12 14:03:59 -0600

    setup new test on non-weighted Gramian

--- offline/drive.f
+++ offline/drive.f
@@ -8,15 +8,20 @@ c-----------------------------------------------------------------------
         call offline_init(icomm)
 
         call blank(fname,132)
-        fname='/Users/kaneko/Developer/MOR/Offline/r0.f00001 '
+        fname='/Users/kaneko/Developer/MOR/offline/r0.f00001 '
+
+        nel=512
 
         ieg(1)=1
-        ieg(2)=2
+        ieg(2)=nel
 
         call setifread(ifread,ldimt)
         write (6,*) 'upt(1)',upt(1)
         call rxupt(xyz,upt,ieg,ifread,fname)
         write (6,*) 'upt(1)',upt(1)
+        call setb(gb,upt,upt,1,512*lxyz)
+
+        write (6,*) 'gb(1)',gb(1)
 
         end
 c-----------------------------------------------------------------------
