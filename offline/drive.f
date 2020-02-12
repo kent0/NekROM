cc4dc21564c22cef52977fafd0d1872e0ed28eac
Kento Kaneko <kaneko2@illinois.edu>
2020-02-12 17:24:06 -0600

    change test

--- offline/drive.f
+++ offline/drive.f
@@ -11,7 +11,7 @@ c-----------------------------------------------------------------------
         call blank(fname,132)
         fname='/Users/kaneko/Developer/MOR/offline/r0.f00001 '
 
-        nel=512
+        nel=1
 
         ieg(1)=1
         ieg(2)=nel
@@ -20,7 +20,15 @@ c-----------------------------------------------------------------------
         write (6,*) 'buf(1)',buf(1)
         call rxupt(buf,ieg,indxr,fname)
         write (6,*) 'buf(1)',buf(1)
-        call setb(gb,buf,buf,1,512*lxyz)
+        call setb(gb,buf,buf,1,nel*lxyz*2)
+
+        do i=1,nel*lxyz
+            write (6,*) i,buf(i),'my_vx'
+        enddo
+
+        do i=1,nel*lxyz
+            write (6,*) i,buf(i+nel*lxyz),'my_vy'
+        enddo
 
         write (6,*) 'gb(1)',gb(1)
 
