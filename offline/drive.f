b4dd7b760793b6275f371c71e08fad82bc0abb56
Kento Kaneko <kaneko2@illinois.edu>
2020-02-18 10:04:04 -0600

    deleted comments

--- offline/drive.f
+++ offline/drive.f
@@ -35,14 +35,6 @@ c-----------------------------------------------------------------------
         mel=1
         nel=mel
 
-c       do ig=1,ng
-c       do is=is0,is1
-c          ieg(1)=nel*(ig-1)+1
-c          ieg(2)=nel*ig
-c          call rxupt(buf(iloc),ieg,indxr,fnames(is))
-c       enddo
-c       enddo
-
         nsg=lsg
         ieg(1)=1
         ieg(2)=1
@@ -51,12 +43,6 @@ c       enddo
         do i=1,lxyz*4
             write (6,*) is,i,ibuf(i),ibuf8(i),'ibuf'
         enddo
-
-c       ieg(1)=1
-c       ieg(2)=1
-
-c       call blank(fname,132)
-c       call rxupt(buf,ieg,indxr,fname)
 
         return
         end
