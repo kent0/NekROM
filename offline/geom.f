e0ab96f5ce4918b631894b0448bcbda15d5f239c
Kento Kaneko <kaneko2@illinois.edu>
2020-02-18 09:54:07 -0600

    removed tabs

--- offline/geom.f
+++ offline/geom.f
@@ -1,18 +1,18 @@
 c-----------------------------------------------------------------------
-		  subroutine setgeom
+      subroutine setgeom
 
-        include 'LVAR'
-        include 'OFFLINE'
-        include 'INTEG'
+      include 'LVAR'
+      include 'OFFLINE'
+      include 'INTEG'
 
-        n=lxyz*nel
+      n=lxyz*nel
 
-        call copy(xm1,xupt,n)
-        call copy(ym1,xupt(n+1),n)
-        if (ldim.eq.3) call copy(zm1,xupt(n*2+1),n)
+      call copy(xm1,xupt,n)
+      call copy(ym1,xupt(n+1),n)
+      if (ldim.eq.3) call copy(zm1,xupt(n*2+1),n)
 
-        call geodat1
+      call geodat1
 
-		  return
-		  end
+      return
+      end
 c-----------------------------------------------------------------------
