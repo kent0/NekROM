8eda6a6225f6892003eec54a282d325fa5a304b6
Kento Kaneko <kaneko2@illinois.edu>
2020-02-19 16:17:57 -0600

    call with correct argument

--- offline/geom.f
+++ offline/geom.f
@@ -14,7 +14,7 @@ c-----------------------------------------------------------------------
 
       call glmapm1(nel)
       call geodat1(nel)
-      call setrxp(rx,nel)
+      call setrxp(nel)
 
       return
       end
