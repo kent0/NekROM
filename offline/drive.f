59bcdd87e95d21c832d5eda757c4be977bbbe9e0
Kento Kaneko <kaneko2@illinois.edu>
2020-02-20 10:55:54 -0600

    fix indentation

--- offline/drive.f
+++ offline/drive.f
@@ -1,42 +1,42 @@
 c-----------------------------------------------------------------------
-        program offline
+      program offline
 
-        include 'LVAR'
-        include 'OFFLINE'
+      include 'LVAR'
+      include 'OFFLINE'
 
-        common /nekmpi/ mid,mp,nekcomm,nekgroup,nekreal
-        character*132 fname
+      common /nekmpi/ mid,mp,nekcomm,nekgroup,nekreal
+      character*132 fname
 
-        call offline_init(icomm)
+      call offline_init(icomm)
 
-        nsg=lsg
-        nsg=5
+      nsg=lsg
+      nsg=5
 
-        ie=1
+      ie=1
 
-        nel=2
-        mel=1
-        ng=neg/mel
-        ng=1
+      nel=2
+      mel=1
+      ng=neg/mel
+      ng=1
 
-        do i=1,ng
-           write (6,*) i,'offline'
-           ieg(1)=ie
-           ieg(2)=ie+mel-1
-           nel=ieg(2)-ieg(1)+1
+      do i=1,ng
+         write (6,*) i,'offline'
+         ieg(1)=ie
+         ieg(2)=ie+mel-1
+         nel=ieg(2)-ieg(1)+1
 
-           call loadsnaps(buf,ieg,indxr,nsg)
-           call setgeom(buf,nel)
-           call setops(ga,gb,gc,gt,buf(nel*lxyz*ldim+1),nel,nsg,ldim)
-           ie=ie+mel
-        enddo
+         call loadsnaps(buf,ieg,indxr,nsg)
+         call setgeom(buf,nel)
+         call setops(ga,gb,gc,gt,buf(nel*lxyz*ldim+1),nel,nsg,ldim)
+         ie=ie+mel
+      enddo
 
-        call gop(ga,gt,'+  ',nsg*nsg)
-        call gop(gb,gt,'+  ',nsg*nsg)
+      call gop(ga,gt,'+  ',nsg*nsg)
+      call gop(gb,gt,'+  ',nsg*nsg)
 
-        call dump_serial(ga,nsg*nsg,'ga ',mid)
-        call dump_serial(gb,nsg*nsg,'gb ',mid)
+      call dump_serial(ga,nsg*nsg,'ga ',mid)
+      call dump_serial(gb,nsg*nsg,'gb ',mid)
 
-        return
-        end
+      return
+      end
 c-----------------------------------------------------------------------
